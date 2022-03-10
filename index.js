f83e188b2b82ccf4990b7ab90e4d55983b4086e2
Muhtacin Manik <gq0067@wayne.edu>
2022-03-09 20:44:56 -0500

    inital registration setup

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //Libraries
-const express = require("express");
 const axios = require("axios");
-const path = require("path");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const bodyParser = require("body-parser");
+const express = require("express");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sportsocialmediaprofile-default-rtdb.firebaseio.com"
+});
+
+const csrfMiddleware = csrf({ cookie: true });
 
 // Settings
 const app = express();
@@ -13,6 +25,17 @@ app.set("view engine", "ejs");
 
 //Folder Path
 app.use(express.static(__dirname + "/public"));
+
+//Settings for 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
 
 //Home Page
 app.get("/", (req, res) => {
@@ -49,4 +72,56 @@ app.get("/teams/:teamName", (req, res) => {
   };
 
   getTeam();
+});
+
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+app.get("/profile", function (req, res) {
+  const sessionCookie = req.cookies.session || "";
+  const type = req.query.type || "none";
+
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true)
+    .then((userData) => {
+      res.render("profile", { userData });
+    })
+    .catch((error) => {
+      res.redirect("/login?type=" + type);
+    });
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+});
+
+app.get("/signout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/");
+});
+
+app.get("/forgot", (req, res) => {
+  res.render("forgot");
 });
