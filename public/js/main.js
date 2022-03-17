94711fa91a76ed3e73280649bb1c367cce14f075
Miqdad Hafiz <gp6062@wayne.edu>
2022-03-16 20:15:08 -0400

    Filter cetnering and css fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
-$('#filterC').change(function () {
-    var value = $('#filterC :selected').text();
-    $('.teamCard').hide();
-    $('[data-conference="' + value + '"]').show();
+$('#filterC').change(function () { // Conference filter function
+    // created variable to get the selected text from drop down menu
+    var value = $('#filterC :selected').text(); 
+    $('.teamCard').hide(); // hide everything that doesn't have a data value equal to the selected option
+    $('[data-conference="' + value + '"]').show(); // show teams that have selected data value
 });
 
-$('#filterD').change(function () {
-    var value1 = $('#filterD :selected').text();
-    $('.teamDiv').hide();
-    $('[data-division="' + value1 + '"]').show();
+$('#filterD').change(function () { // DIvision filter function
+    var value1 = $('#filterD :selected').text(); // grab the selected option
+    $('.teamDiv').hide(); // hide everything else
+    $('[data-division="' + value1 + '"]').show(); // show the teams with data value equal to the selected option
 });
