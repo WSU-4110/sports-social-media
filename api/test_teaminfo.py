c3e8b206d94cb582866b7959c1ac10a2e702de5b
Muhtacin Manik <gq0067@wayne.edu>
2022-04-20 18:57:15 -0400

    team info unittest

---
+++ api/test_teaminfo.py
@@ -0,0 +1,42 @@
+from os import name
+import unittest
+from modules.gsearch import get_individual_teams
+
+class TruthTest(unittest.TestCase):
+    def test_for_500(self):
+        actual=get_individual_teams("weee")
+        expected=500
+        self.assertEqual(actual, expected)
+
+    def test_for_200(self):
+        actual=get_individual_teams(1)[0]
+        expected=200
+        self.assertEqual(actual, expected)
+
+    def test_for_team_name(self):
+        actual=get_individual_teams(1)[1]
+        actual=actual['team']['displayName']
+        expected="Atlanta Hawks"
+        self.assertEqual(actual, expected)
+
+    def test_for_team_color(self):
+        actual=get_individual_teams(1)[1]
+        actual=actual['team']['color']
+        expected="002B5C"
+        self.assertEqual(actual, expected)
+
+    def test_for_team_players(self):
+        actual=get_individual_teams(1)[1]
+        actual=actual['athletes']
+        expected=""
+        self.assertIsNot(actual, expected)
+
+    def test_for_team_logo(self):
+        actual=get_individual_teams(1)[1]
+        actual=actual['team']['logo']
+        expected=""
+        self.assertIsNot(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
